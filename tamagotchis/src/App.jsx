38de69e5697efa492a48d6a294a390637adb3df9
Marleigh Ausbrooks <ma108@wellesley.edu>
2025-01-14 16:18:38 -0500

    adding in a bunch of images

--- tamagotchis/src/App.jsx
+++ tamagotchis/src/App.jsx
@@ -3,9 +3,22 @@ import "./App.css";
 
 const tamagotchiData =
 [{
+  name:"Tamagotchi Pix",
+  facts:[
+    "this was my first ever tamagotchi model!",
+    "it's broken down into the traditional pix, and then the pix party",
+    "i bought the pix party in the pink & blue!",
+  ],
+  images:[
+    "./assets/Tamagotchi1.png",
+    "./assets/Tamagotchi2.png"
+  ]
+  
+},
+{
   name:"Tamagotchi Uni",
   facts:[
-    "I own two: the original purple and pink",
+    "I have two! Both from the original wave when the model was first released",
     "I have yet to run the purple one, as my uncle got it for me in Japan"
   ],
   images:[
@@ -14,6 +27,7 @@ const tamagotchiData =
   ]
   
 }
+
 ]
 
 function App() {
@@ -28,8 +42,8 @@ function App() {
           </div>)}
 
       <div className="card">
-            <img src= {"./assets/Tamagotchi1.png"}></img>
-            <img src={"./assets/Tamagotchi2.png"}></img>
+            <img src= "./assets/Tamagotchi1.png"></img>
+            <img src="./assets/Tamagotchi2.png" ></img>
             <img
               src="./assets/Tamagotchi3.png"
             ></img>
