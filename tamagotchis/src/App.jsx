9d8bc08679797a49bcac5faf750d4e5b3a592010
Marleigh Ausbrooks <ma108@wellesley.edu>
2025-01-14 11:43:09 -0500

    created a basic mapping for writing about my tamagotchis! images pending

--- tamagotchis/src/App.jsx
+++ tamagotchis/src/App.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 
+const tamagotchiData =
+[{
+  name:"Tamagotchi Uni",
+  facts:[
+    "I own two: the original purple and pink",
+    "I have yet to run the purple one, as my uncle got it for me in Japan"
+  ],
+  images:[
+    "./assets/Tamagotchi1.png",
+    "./assets/Tamagotchi2.png"
+  ]
+  
+}
+]
+
 function App() {
   return (
     <>
-      <h1> Marleigh's Tamagotchi Time ! </h1>
+      <h2> Marleigh's Tamagotchi Time ! </h2>
+      {tamagotchiData.map(tamagotchiModel => <div key={tamagotchiModel.name}>
+        <h3>{tamagotchiModel.name}</h3>
+        <ul>
+          {tamagotchiModel.facts.map(f => <li>{f}</li>)}
+        </ul>
+          </div>)}
+
       <div className="card">
-            <img src="assets/Tamagotchi1.png" alt="Snow" style="width:100%"></img>
-            <img src="assets/Tamagotchi2.png" alt="Forest" style="width:100%"></img>
+            <img src= {"./assets/Tamagotchi1.png"}></img>
+            <img src={"./assets/Tamagotchi2.png"}></img>
             <img
-              src="assets/Tamagotchi3.png"
-              alt="Mountains"
-              style="width:100%"
+              src="./assets/Tamagotchi3.png"
             ></img>
           </div>
     </>
